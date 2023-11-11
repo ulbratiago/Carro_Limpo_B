9124e6e87c88537fe4ecd78c5eabb86f2f5406cd
Tiago Raupp <tiraupp@gmail.com>
2023-11-11 17:02:09 -0300

    fix: ajuste esquemas login

--- src/esquemas/login.esquemas.js
+++ src/esquemas/login.esquemas.js
@@ -7,12 +7,10 @@ const loginEsquema = joi.object({
     'string.base': 'O campo email precisa ter um formato válido',
     'string.email': 'O campo email precisa ter um formato válido',
   }),
-  senha: joi.string().min(8).max(15).required().messages({
+  senha: joi.string().required().messages({
     'any.required': 'O campo senha é obrigatório',
     'string.empty': 'O campo senha é obrigatório',
     'string.base': 'O campo senha deve conter caracteres válidos',
-    'string.min': 'A senha deve possuir no mínimo 8 caracteres',
-    'string.max': 'A senha deve possuir no máximo 15 caracteres',
   }),
 })
 
